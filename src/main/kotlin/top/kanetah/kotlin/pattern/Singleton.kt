4ac4489c2a446738461074c84524a9fad80e676e
kanetah <2205485120@qq.com>
2018-03-19 18:59:41 +0800

    Singleton

--- src/main/kotlin/top/kanetah/kotlin/pattern/Singleton.kt
+++ src/main/kotlin/top/kanetah/kotlin/pattern/Singleton.kt
@@ -1,8 +1,35 @@
 package top.kanetah.kotlin.pattern
 
+import org.junit.Test
 
 /*
  * created by kane on 2018/3/19
  * 单例模式
  */
-object Singleton
+
+interface Printable {
+    fun print()
+}
+
+object Singleton : Printable {
+
+    init {
+        println("init object ${this::javaClass.name}")
+    }
+
+    override fun print() {
+        println(this)
+    }
+}
+
+class SingletonTest {
+    @Test
+    fun test() {
+        println("--- start ---")
+        Singleton // 此对象为单例
+                .print()
+        println("--- join ---")
+        Singleton.print()
+        println("--- end ---")
+    }
+}
